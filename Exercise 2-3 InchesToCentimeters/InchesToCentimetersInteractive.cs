c2b5de49ea04174cfee772b25c3249aff663f931
Benjamin Goff <bgoff@mainerobotics.org>
2020-12-16 09:40:10 -0500

    Add project files.

---
+++ Exercise 2-3 InchesToCentimeters/InchesToCentimetersInteractive.cs
@@ -0,0 +1,23 @@
+﻿using static System.Console;
+using System;
+/*
+ * Exercise C# 2-3 Inches to Centimeters
+ * 12/16/2020
+ * Prompts user for a measurement in inches and converts to centimeters
+ */
+namespace Exercise_2_3_InchesToCentimeters
+{
+    class InchesToCentimetersInteractive
+    {
+        static void Main(string[] args)
+        {
+            const double CENTIMETERS_PER_INCH = 2.54;
+
+            WriteLine("Measurement in inches: "); // prompt for the user
+
+            double inches = Convert.ToDouble(ReadLine()); //get user input and convert to double
+
+            WriteLine("{0} inches is {1} centimeters", inches, inches * CENTIMETERS_PER_INCH);
+        }
+    }
+}
