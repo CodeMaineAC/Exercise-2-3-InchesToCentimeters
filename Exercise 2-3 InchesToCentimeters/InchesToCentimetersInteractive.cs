917074b7155871191527c3cba5652b4f31a52d5c
Benjamin Goff <bgoff@mainerobotics.org>
2020-12-16 10:22:13 -0500

    This example uses TryParse to check to see if user input is of a valid type.

--- Exercise 2-3 InchesToCentimeters/InchesToCentimetersInteractive.cs
+++ Exercise 2-3 InchesToCentimeters/InchesToCentimetersInteractive.cs
@@ -1,9 +1,12 @@
 ﻿using static System.Console;
 using System;
 /*
- * Exercise C# 2-3 Inches to Centimeters
+ * Exercise C# 2-3 Inches to Centimeters v2
  * 12/16/2020
- * Prompts user for a measurement in inches and converts to centimeters
+ * Prompts user for a measurement in inches and converts to centimeters. 
+ * 
+ * This example uses TryParse to check to see if user input is of a valid
+ * type.
  */
 namespace Exercise_2_3_InchesToCentimeters
 {
@@ -13,11 +16,31 @@ namespace Exercise_2_3_InchesToCentimeters
         {
             const double CENTIMETERS_PER_INCH = 2.54;
 
-            WriteLine("Measurement in inches: "); // prompt for the user
+            Write("Measurement in inches: "); // prompt for the user
+            bool notParsed = true;
+            
+            while(notParsed)
+            {
+                /*
+                 * TryParse checks to see if a string can be converted to a particular Data Type
+                 * if it returns true it will also output the value of the data type it is converting to.
+                 * 
+                 * In this example the TryParse check to see if the user input can be converted to a
+                 * double, and if that is true, it outputs the results to the variable inches.
+                 */
+                if (Double.TryParse(ReadLine(), out double inches)) 
+                {
+                    WriteLine("{0} inches is {1} centimeters", inches, inches * CENTIMETERS_PER_INCH);
+                    notParsed = false;
+                }
+                else
+                {
+                    WriteLine("Incorrect input, try again");
+                    Write("Measurement in inches: ");
+                }
+            }
 
-            double inches = Convert.ToDouble(ReadLine()); //get user input and convert to double
-
-            WriteLine("{0} inches is {1} centimeters", inches, inches * CENTIMETERS_PER_INCH);
+           
         }
     }
 }
